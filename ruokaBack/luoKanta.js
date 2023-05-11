3b42134c6031f294c2459dfb5b1c7601f713d51c
Dovile Mart <doviliu@gmail.com>
2023-05-11 10:57:41 +0300

    tietokanta, palvelin

---
+++ ruokaBack/luoKanta.js
@@ -0,0 +1,128 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("ruoka.db");
+
+db.serialize(() => {
+  let sql = "CREATE TABLE laatija (" +
+    "idl integer PRIMARY KEY NOT NULL, " +
+    "etunimi text NOT NULL, " +
+    "paiva date, " +
+    "kuva text)";
+    
+  db.run(sql, (err) => {
+    if (err) {
+      return console.log('laatija-luonti' + err.message);
+    }
+    console.log("Laatija-taulu luotu");
+  })
+
+  sql = "INSERT INTO `laatija` (`idl`,`etunimi`, `paiva`, `kuva`) " +
+    " VALUES "
+          + "(1, 'Minna', '15-2-2023', 'kuva1.jpg'),"
+          + "(2, 'Iina', '18-2-2023', 'kuva2.jpg'),"
+          + "(3, 'Matti', '15-1-2023', 'kuva3.png')";
+  
+  db.run(sql, (err) => {
+    if (err) {
+      return console.log('Laatija-tauluun lisäys '+ err.message);
+    }
+    console.log("Laatijarivit lisättiin");
+  })
+
+  sql = "CREATE TABLE resepti (" +
+        "id integer PRIMARY KEY NOT NULL, " +
+        "nimi text NOT NULL, " +
+        "kuva text, " +
+        "kuvaus text NOT NULL, " +
+        "kesto integer NOT NULL, " +
+        "ainekset text NOT NULL, " +
+        "ohje text NOT NULL, " +
+        "idl integer," +
+        "FOREIGN KEY (idl) REFERENCES laatija (idl) )";
+      
+  db.run(sql, (err) => {
+  if (err) {
+    return console.log('resepti-luonti' + err.message);
+  }    console.log("Resepti-taulu luotu");
+  }) 
+      
+  sql = "INSERT INTO `resepti` (`nimi`, `kuva`, `kuvaus`, `kesto`, `ainekset`, `ohje`, 'idl') " +
+      " VALUES "
+      + "('Tomaattinen linssi-palvikinkkupata', 'linssipata.jpg', 'Maukas arkiruoka valmistuu kuivaruokakaapin tarvikkeista, säilykkeistä ja jääkaapissa hyvin säilyvistä tuotteista. Muhevuudestaan huolimatta, tämä ruokaisa pata ei vaadi pitkää haudutusaikaa. Pata maistuu maalaisleivän kanssa nautittuna tai lisukeriisin kanssa.', 15, '250g savukylkeä, 150 g sipulia hienonnettua, 2rkl vehnäjauhoja, 400g säilyketomaatteja, 380g esikeitettyjä punaisia linssejä, 2,5dl ruokakermaa, 1 kasvisliemikuutio, 35g suolakurkkua kuutioituna, ½tl sokeria, kuivattua ruohosipulia tai persiljaa.', 'Paloittele savukylki. Ruskista palat isossa valurautapadassa tai kattilassa kannen alla, lisää loppuvaiheessa hienonnettu sipuli. HUOM! Noudata erityistä varovaisuutta lihan ruskistamisessa ja käytä kantta. Liha ja rasva räiskyy voimakkaasti. Ennen sipulin lisäämistä, nosta astia sivuun sekoita ja sulje taas kansi. Ripottele joukkoon vehnäjauho. Kääntele hetki. Lisää joukkoon säilyketomaatit, huuhtele tölkit tilkkasella vettä. Lisää myös linssit, ruokakerma ja liemikuutio. Anna kiehua hiljalleen kannen alla 5 min. Jos käytät kuivattuja linssejä, huuhtele ne siivilässä ennen lisäämistä ja tarkista pakkauksesta kypsymisaika. Kuivatut linssit eivät sisällä suolaa, joten käytä silloin kokonainen liemikuutio ruokaan. Lisää suolakurkut ja mausta sokerilla. Viimeistele ruohosipulilla tai persiljalla. Tarjoa pata keitetyn riisin tai maalaisleivän kanssa.', 1),"//
+      + "('Pestopasta', 'pestopasta.jpg', 'Joskus yksinkertaisuus on valttia. Tämän herkullisen pestopastan salaisuus on itse tehdyssä pestossa.', 30, '500g tuorepastaa, PESTO: 2 ruukkua tuoretta basilikaa, 2 valkosipulin kynttä, 75g cashewpähkinöitä, 1½dl oliiviöljyä, ½tl suolaa, 1½dl	Mustaleima raastetta. PÄÄLLE: ½dl Mustaleima raastetta, tuoretta basilikaa', 'Valmista pesto: Mittaa ainekset blenderiin tai mittakannuun. Soseuta blenderillä tai sauvasekoittimella, kunnes peston koostumus on sopivaa. Mausta suolalla ja pippurilla. Keitä pasta runsaassa suolalla maustetussa vedessä pakkauksen ohjeen mukaan. Sekoita pesto valutetun, kuuman pastan joukkoon. Viimeistele annokset juustoraasteella ja basilikalla. Tarjoa heti.', 1),"//
+      + "('Unelmarahkatorttu', 'rahkatorttu.jpg', 'Unelmatorttu raikkaalla ja sitruunaisella rahka-kreemitäytteellä. Kääretorttu on vaivaton valmistaa ja siitä riittää isommallekkin porukalle herkutella.', 45, 'POHJA: 4 kananmunaa, 1½dl sokeria, 1dl perunajauhoja, ½dl kaakaojauhetta, 1tl leivinjauhetta. TÄYTE: 200g vaniljakreemiä, 200g sitruunarahkaa.', 'Vatkaa huoneenlämpöiset munat ja sokeri paksuksi, vaaleaksi vaahdoksi. Yhdistä kuivat aineet. Lisää ne siivilän läpi vaahtoon varovasti sekoittaen. Levitä taikina leivinpaperilla vuoratulle uunipellille. Paista 200 asteessa n. 8 min tai kunnes kypsä. Kumoa pohja sokerilla sirotetulle leivinpaperille. Irrota pohjapaperi. Sekoita kreemi ja rahka. Levitä seos jäähtyneelle kääretorttupohjalle. Kääri rullaksi. Anna tekeytyä hetki kylmässä ja leikkaa paloiksi.', 2),"//
+    + "('Täytetyt avokadot', 'avocado.jpg', 'Katkaravuilla ja homejuustolla täytetyt avokadot.', 20, '1 avokado, 20 katkaravunpyrstöä, 2 rkl kermaviiliä, 2 rkl majoneesia, 2rkl sinihomejuustoa.', 'Leikkaa avokadot pitkittäin kahtia ja poista kivi. Täytä kolo katkaravuilla. Murusta homejuusto ja sekoita joukkoon kermaviili sekä majoneesi. Valuta kastike katkaravuille ja avokadonpuolikkaille.', 3)";
+  
+  db.run(sql, (err) => {
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log("Reseptit lisättiin");
+  });
+  
+  db.each("SELECT idl, etunimi, paiva FROM laatija", function (err, row) {
+      if (err) {
+          return console.log(err.message);
+        }
+        console.log(row.idl + ", " + row.etunimi + ", " + row.paiva);
+      });
+
+  db.close();
+})
+
+ //tuli ilmoitus että ambiguous(epäselvä) idl
+ // sql = "SELECT nimi, etunimi FROM resepti LEFT JOIN laatija ON laatija.idl=resepti.idl";
+
+//  db.each("SELECT id, nimi FROM resepti", function (err, row) {
+/*  db.each("SELECT idl, idl FROM resepti INNER JOIN laatija ON laatija.idl=resepti.idl", function (err, row) {
+  if (err) {
+      return console.log(err.message);
+    }
+    console.log(row.resepti.id + ", " + row.resepti.nimi + ", " + row.laatija.idl+", "+row.laatija.etunimi);
+  });*/
+
+/**, " +
+        "idl integer NOT NULL, " +
+        "FOREIGN KEY(idl) REFERENCES laatija(idl)) */
+
+
+   //parallelize
+//  db.run("CREATE TABLE laatija");
+//  db.run("CREATE TABLE resepti");
+
+/*	sql = "INSERT INTO `resepti` (`id`, `nimi`, `kuva`, `kuvaus`, `kesto`, `ainekset`, `ohje`) "+
+    " VALUES (1, '', '', '', , '', '')";
+    db.run(sql, (err) => {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log("Resepti lisättiin");
+    })
+  */
+
+      /*  sql = "INSERT INTO `resepti` (`nimi`, `kuva`, `kuvaus`, `kesto`, `ainekset`, `ohje`) "+
+      " VALUES ('Pestopasta', 'pestopasta.jpg', 'Joskus yksinkertaisuus on valttia. Tämän herkullisen pestopastan salaisuus on itse tehdyssä pestossa.', 30, '500g tuorepastaa, PESTO: 2 ruukkua tuoretta basilikaa, 2 valkosipulin kynttä, 75g cashewpähkinöitä, 1½dl oliiviöljyä, ½tl suolaa, 1½dl	Mustaleima raastetta. PÄÄLLE: ½dl Mustaleima raastetta, tuoretta basilikaa', 'Valmista pesto: Mittaa ainekset blenderiin tai mittakannuun. Soseuta blenderillä tai sauvasekoittimella, kunnes peston koostumus on sopivaa. Mausta suolalla ja pippurilla. Keitä pasta runsaassa suolalla maustetussa vedessä pakkauksen ohjeen mukaan. Sekoita pesto valutetun, kuuman pastan joukkoon. Viimeistele annokset juustoraasteella ja basilikalla. Tarjoa heti.')";
+      db.run(sql, (err) => {
+          if (err) {
+            return console.log(err.message);
+          }
+          console.log("Resepti 2 lisättiin");
+      })
+    
+      sql = "INSERT INTO `resepti` (`nimi`, `kuva`, `kuvaus`, `kesto`, `ainekset`, `ohje`) "+
+      " VALUES ('Unelmarahkatorttu', 'rahkatorttu.jpg', 'Unelmatorttu raikkaalla ja sitruunaisella rahka-kreemitäytteellä. Kääretorttu on vaivaton valmistaa ja siitä riittää isommallekkin porukalle herkutella.', 45, 'POHJA: 4 kananmunaa, 1½dl sokeria, 1dl perunajauhoja, ½dl kaakaojauhetta, 1tl leivinjauhetta. TÄYTE: 200g vaniljakreemiä, 200g sitruunarahkaa.', 'Vatkaa huoneenlämpöiset munat ja sokeri paksuksi, vaaleaksi vaahdoksi. Yhdistä kuivat aineet. Lisää ne siivilän läpi vaahtoon varovasti sekoittaen. Levitä taikina leivinpaperilla vuoratulle uunipellille. Paista 200 asteessa n. 8 min tai kunnes kypsä. Kumoa pohja sokerilla sirotetulle leivinpaperille. Irrota pohjapaperi. Sekoita kreemi ja rahka. Levitä seos jäähtyneelle kääretorttupohjalle. Kääri rullaksi. Anna tekeytyä hetki kylmässä ja leikkaa paloiksi.')";
+      db.run(sql, (err) => {
+          if (err) {
+            return console.log(err.message);
+          }
+          console.log("Resepti 3 lisättiin");
+      })
+        
+      sql = "INSERT INTO `resepti` (`nimi`, `kuva`, `kuvaus`, `kesto`, `ainekset`, `ohje`) "+
+      " VALUES ('Täytetyt avokadot', 'avocado.jpg', 'Katkaravuilla ja homejuustolla täytetyt avokadot.', 20, '1 avokado, 20 katkaravunpyrstöä, 2 rkl kermaviiliä, 2 rkl majoneesia, 2rkl sinihomejuustoa.', 'Leikkaa avokadot pitkittäin kahtia ja poista kivi. Täytä kolo katkaravuilla. Murusta homejuusto ja sekoita joukkoon kermaviili sekä majoneesi. Valuta kastike katkaravuille ja avokadonpuolikkaille.')";
+      db.run(sql, (err) => {
+          if (err) {
+            return console.log(err.message);
+          }
+          console.log("Resepti 4 lisättiin");
+      })
+    */
---
+++ ruokaBack/ruokaServer.js
@@ -0,0 +1,189 @@
+const express = require('express');
+const app = express();
+
+var helmet = require('helmet');
+app.use(helmet( { crossOriginResourcePolicy: false } ));
+
+app.use(express.json()); 
+app.use(express.urlencoded( { limit: '5mb', extended: true } ));
+
+const cors = require('cors');
+app.use(cors());
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('ruoka.db');
+
+app.listen(8080, () => {
+	console.log('Node toimii localhost:8080');
+});
+
+//OK
+app.get('/', (req, res, next) => {
+	return res.status(200).json( { error: false, message: 'Toimii' } );
+});
+
+//OK
+app.get('/resepti/all', (req, res, next) => {
+		db.all('SELECT * FROM resepti', (error, results) => {
+		if(error) throw error;
+		console.log('tietokannasta haettussa resepti-taulukkossa on ' + results.length + ' objektia');
+            return res.status(200).json(results);
+		}) // db.all
+}) // app.get
+
+//OK
+app.get('/resepti/one/:id', (req, res, next) => {
+	let id = req.params.id;
+
+	db.get('SELECT * FROM resepti where id=?', [id], (error, result) => {
+		if (error) throw error;
+        if (typeof (result) == 'undefined') {
+            console.log('tietokannassa ei ole objektia id:llä ' + id);
+            return res.status(200).json({});
+        }
+        console.log('tietokannasta haettu objekti id:llä ' + result.id);
+        return res.status(200).json(result);
+	}) // db.get
+}) // app.get
+
+//kuvan lataaminen, postmanin kautta testaus OK
+const multer = require('multer');
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, './images'); // Mihin kansioon ladataan
+	},
+	filename: (req, file, callback) => {
+		callback(null, file.originalname);  // Millä tiedostonimellä
+	}
+});
+const upload = multer({ storage: storage })
+
+app.post('/resepti/add', upload.single('kuva'), (req, res, next) => {
+	let resepti = req.body;
+	let laatija = req.body;
+	let kuvaNimi = null;
+	if (req.file) {
+		kuvaNimi = req.file.originalname;
+	}
+	//db.serialize(()=>{
+		db.run('INSERT INTO resepti (nimi,kuva,kuvaus,kesto,ainekset,ohje,idl) VALUES (?, ?, ?, ?, ?, ?,?)',
+			[resepti.nimi, kuvaNimi, resepti.kuvaus, resepti.kesto, resepti.ainekset, resepti.ohje, laatija.idl], (error, result) => {
+				if (error) throw error;
+			return res.status(200).json({ count: 1 });
+		});
+	//})
+})
+
+app.post('/laatija/add', upload.single('kuva'), (req, res, next) => {
+	let laatija = req.body;
+	let kuvaNimi = null;
+	if (req.file) {
+		kuvaNimi = req.file.originalname;
+	}
+	db.run('INSERT INTO laatija (etunimi,paiva,kuva) VALUES (?, ?, ?)',
+		[laatija.etunimi, laatija.paiva, kuvaNimi], (error, result) => {
+			if (error) throw error;
+			console.log('Laatija lisätty')
+		return res.status(200).json({ count: 1 });
+	});
+})
+
+
+app.get('/download/:nimi', (req, res, next) => {
+	let file = './images/' + req.params.nimi;
+	res.download(file);
+});
+
+//OK
+app.get('/resepti/delete/:id', (req, res, next) => {
+	let id = req.params.id;
+	db.run('DELETE FROM resepti WHERE id = ?', [id], function (error, result) {
+		console.log('Yritetään poistaa resepti id:llä: ' + id);
+		if (error) throw error;
+		return res.status(200).json({ count: this.changes });
+	})
+})
+
+//muokkaus ei toimiva
+app.put('/resepti/edit/:id', (req, res, next) => {
+	let id = req.params.id;
+	let resepti = req.body;
+	db.run('INSERT INTO resepti (nimi) VALUES (?)', [id], [resepti.nimi], (error, result)=> {
+		console.log('Yritetään muokata reseptin tietoja, id: ' + id);
+		if (error) throw error;
+		return res.status(200).json( {count: this.changes} );
+	})
+})
+
+//OK
+app.get('/laatija/all', (req, res, next) => {
+	db.all('SELECT * FROM laatija', (error, results) => {
+		if (error) throw error;
+		console.log('tietokannasta haettussa laatija-taulukkossa on ' + results.length + ' objektia');
+		return res.status(200).json(results);
+	}) // db.all
+}) // app.get
+
+//OK
+app.get('/laatija/one/:idl', (req, res, next) => {
+	let id = req.params.idl;
+	db.get('SELECT * FROM laatija where idl=?', [id], (error, result) => {
+		if (error) throw error;
+		if (typeof (result) == 'undefined') {
+            console.log('tietokannassa ei ole objektia id:llä ' + id);
+            return res.status(200).json({});
+		}
+		console.log('tietokannasta haettu objekti id:llä ' + id);//result.idl
+		return res.status(200).json(result);
+	}) // db.get
+}) // app.get
+
+//OK
+app.get('/laatija/delete/:id', (req, res, next) => {
+	let id = req.params.id;
+	db.run('DELETE FROM laatija WHERE idl = ?', [id], function (error, result) {
+		console.log('Yritetään poistaa laatija id:llä: ' + id);
+		if (error) throw error;
+		return res.status(200).json( {count: this.changes} );
+	})
+})
+
+//muokkaus NYT SE LUO UUDEN  [object FormData]-nimellä
+app.put('/laatija/edit/:id', (req, res, next) => {
+	let id = req.params.id;
+	let laatija = req.body;
+	db.run('INSERT INTO laatija (etunimi) VALUES (?)', [id], [laatija.etunimi], (error, result)=> {
+		console.log('Yritetään muokata laatijan tietoja, id: ' + id);
+		if (error) throw error;
+		return res.status(200).json( {count: this.changes} );
+	})
+})
+
+// kaikki yhden laatijan reseptit, toimii mutta ei poimi etunimeä
+app.get('/laatija/one/:idl/reseptit', (req, res, next) => {
+	let id = req.params.idl;
+	db.all('SELECT * FROM resepti WHERE idl=?', [id], (error, results) => {
+		if (error) throw error;
+
+		console.log('resepti-taulukkossa ' + results.length + ' resepti/-ia, joita on luonut laatija ' + id);
+		return res.status(200).json(results);
+	}) // db.all
+}) // app.get
+//TAI
+app.get('/resepti/all/:idl', (req, res, next) => {
+	let id = req.params.idl;
+	db.all('SELECT * FROM resepti WHERE idl=?', [id], (error, results) => {
+		if (error) throw error;
+		console.log('resepti-taulukkossa on laatijan ' + id + ' reseptit: ' + results.length);
+		console.log(results.toString(id.nimi));
+		return res.status(200).json(results);
+	}) // db.all
+}) // app.get
+
+
+
+
+//OK
+app.get('*', (req, res, next) => {
+	return res.status(404).json( { error: true, message: 'Ei pyydettyä palvelua' } );
+})
